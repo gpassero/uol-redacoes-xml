3d5ffd129102075e3e9d4b89c7a6b68c2a0ee61b
Guilherme Passero <guilherme.passero0@gmail.com>
2017-05-12 16:11:48 -0300

    Adicionado pacote principal ao setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='uol_redacoes_xml',
 	  author='Guilherme Passero',
 	  author_email='guilherme.passero0@gmail.com',
 	  url='https://github.com/gpassero/uol-redacoes-xml',
-      packages=['uol_redacoes_xml.reader', 'uol_redacoes_xml.crawler']
+      packages=['uol_redacoes_xml', 'uol_redacoes_xml.reader', 'uol_redacoes_xml.crawler']
 )
